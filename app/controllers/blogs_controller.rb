5b5cd3bcf57af0849400e356ab6d6a55a6da7d3b
Nayela Wickramasuriya <nayelawick@gmail.com>
2018-12-25 19:13:14 +0530

    Added draft status toggle feature for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
  class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -57,6 +57,16 @@
       format.html { redirect_to blogs_url, notice: 'Blog post successfully deleted.' }
       format.json { head :no_content }
     end
+  end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+
+    redirect_to blogs_url, notice: "Blog post status has been updated"
   end
 
   private
